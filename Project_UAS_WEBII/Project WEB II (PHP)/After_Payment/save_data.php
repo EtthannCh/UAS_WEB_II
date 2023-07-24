282981b044e6f045fafabcd13488aea9727e77de
EtthannCh <107177413+EtthannCh@users.noreply.github.com>
2023-07-24 21:23:30 +0700

    Update save_data.php

--- Project_UAS_WEBII/Project WEB II (PHP)/After_Payment/save_data.php
+++ Project_UAS_WEBII/Project WEB II (PHP)/After_Payment/save_data.php
@@ -3,7 +3,7 @@ include_once "../signup/config.php";
 include_once "../lib.php";
 
 echo $_POST["kode"];
-echo insertToTransaksi("Menunggu Konfirmasi", $_POST["kode"], date("Y/m/d"));
+insertToTransaksi("Menunggu Konfirmasi", $_POST["kode"], date("Y/m/d"));
 deleteUserCart($_SESSION["id_user"]);
 
 ?>
